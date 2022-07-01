61dc95483858df11c776e7b763f4de06e1f80335
hasnentai <hasnen712@live.com>
2022-07-01 12:24:39 +0530

    Added Theme and Componants

---
+++ src/components/Buttons.js
@@ -0,0 +1,51 @@
+import { Button, Box } from "native-base";
+
+const NativeBaseHackButton = ({
+  onPress,
+  label,
+  size,
+  colorScheme,
+  isDisabled = false,
+  isLoading = false,
+  isLoadingText = "Submitting...",
+  leftIcon,
+  variant = "solid",
+}) => {
+  return (
+    <Box alignItems="center">
+      <Button
+        isLoading={isLoading}
+        isLoadingText={isLoadingText}
+        onPress={onPress}
+        isDisabled={isDisabled}
+        colorScheme={colorScheme}
+        variant={variant}
+        size={size}
+        leftIcon={leftIcon}
+        m={1}
+      >
+        {label}
+      </Button>
+    </Box>
+  );
+};
+
+const NativeBaseHackButtonGroup = ({ label }) => {
+  return (
+    <Button.Group
+      m={1}
+      isAttached
+      mx={{
+        base: "auto",
+        md: 0,
+      }}
+      size="sm"
+    >
+      <Button variant={"outline"}>{label[0]}</Button>
+      <Button variant={false ? "outline" : "solid"}>{label[1]}</Button>
+      <Button variant={"outline"}>{label[1]}</Button>
+    </Button.Group>
+  );
+};
+
+export { NativeBaseHackButton, NativeBaseHackButtonGroup };
